a397732e6995db95155e503cc1ff5c8f5db5fbcb
doan hoai nam <doanhoainam09022005123@gmail.com>
2024-10-28 23:00:58 +0700

    Bài tập chương 2

--- Chuong_2/bai2.4.py
+++ Chuong_2/bai2.4.py
@@ -1,8 +1,9 @@
 import xml.dom.minidom
 
 def main():
-    doc=xml.dom.minidom.parse("sample.xml")
+    doc=xml.dom.minidom.parse("Chuong_2/sample.xml")
     print(doc.nodeName)
     print(doc.firstChild.tagName)
+    print(doc.documentElement.nodeName)
 if __name__=="__main__":
     main()
